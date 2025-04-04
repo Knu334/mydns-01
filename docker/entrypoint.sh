a33fadd205d6f655269d417870505dbc0ed2b980
Knu334 <who.is.fuu@gmail.com>
2025-04-04 15:25:57 +0900

    証明書取得済みの場合にrenewするように分岐追加

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-certbot certonly --manual \
-    --preferred-challenges dns \
-    -d *.${CERTBOT_DOMAIN} \
-    -d ${CERTBOT_DOMAIN} \
-    --email ${CERTBOT_EMAIL} \
-    --agree-tos \
-    --manual-auth-hook /app/docker/authenticator.sh \
-    --manual-cleanup-hook /app/docker/cleanup.sh
+if [ -d /etc/letsencrypt/live/${CERTBOT_DOMAIN} ]; then
+    certbot renew
+else
+    certbot certonly --manual \
+        --preferred-challenges dns \
+        -d *.${CERTBOT_DOMAIN} \
+        -d ${CERTBOT_DOMAIN} \
+        --email ${CERTBOT_EMAIL} \
+        --agree-tos \
+        --manual-auth-hook /app/docker/authenticator.sh \
+        --manual-cleanup-hook /app/docker/cleanup.sh
+fi
